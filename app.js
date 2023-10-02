94adda86f6604c4722a792bb56bb4cac5d5b1e71
Lanre <olawunmilanre00@gmail.com>
2023-10-02 18:28:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//const express = require('express');
+//const bodyParser = require('body-parser');
+
+
+//const app = express();
+//const port = process.env.PORT || 3000;
+
+
+//const mongoose = require('mongoose');
+
+
+//require('dotenv').config();
+
+
+//mongoose.connect(process.env.MONGO_URI, {
+//  useNewUrlParser: true,
+  //useUnifiedTopology: true,
+//});
+
+
+//const db = mongoose.connection;
+//db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//db.once('open', () => {
+ // console.log('Connected to the MongoDB database.');
+/});
---
+++ models/person.js
@@ -0,0 +1,71 @@
+//const personSchema = new mongoose.Schema({
+//    name: { type: String, required: true },
+  //  age: Number,
+    //favoriteFoods: [String],
+  //});
+  
+  //const Person = mongoose.model('Person', personSchema);
+
+  //const person = new Person({
+   // name: 'John Doe',
+    //age: 30,
+   // favoriteFoods: ['Pizza', 'Burger'],
+ // });
+  
+  //person.save(function(err, data) {
+    //if (err) return console.error(err);
+   // console.log('Person saved:', data);
+ // });
+
+  //const arrayOfPeople = [
+    //{name: 'Alice', age: 25, favoriteFoods: ['Sushi', 'Pasta'] },
+    //{ name: 'Bob', age: 35, favoriteFoods: ['Steak', 'Chicken'] },
+  
+ 
+  //];
+  
+  //Person.create(arrayOfPeople, function(err, people) {
+    //if (err) return console.error(err);
+   // console.log('People created:', people);
+ // });
+  
+ // Person.find({ name: 'John Doe' }, function(err, people) {
+    //if (err) return console.error(err);
+    //console.log('People with name John Doe:', people);
+  //});
+  
+  //Person.find({ name: 'Bob' }, function(err, people) {
+    //if (err) return console.error(err);
+    //console.log('People with name Bob:', people);
+  //});
+
+  //Person.find({ name: 'Alice' }, function(err, people) {
+    //if (err) return console.error(err);
+    //console.log('People with name Alice:', people);
+  //});
+
+  //Person.findOne({ favoriteFoods: 'Pizza' }, function(err, person) {
+    //if (err) return console.error(err);
+    //console.log('Person who likes Pizza:', person);
+  //});
+
+  
+  //const personId = 'your_person_id_here';
+
+  //const personName = 'John Doe';
+
+//Person.findOneAndUpdate({ name: personName }, { age: 20 }, { new: true }, function(err, updatedPerson) {
+  //if (err) return console.error(err);
+  //console.log('Updated Person:', updatedPerson);
+//});
+
+
+  //const personName = 'john';
+
+  //Person.remove({ name: personName }, function(err, result) {
+    //if (err) return console.error(err);
+    //console.log('Number of People Deleted:', result.deletedCount);
+  //});
+
+  
+
